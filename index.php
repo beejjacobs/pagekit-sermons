4f0fefbb7b715cccbd705441065029c54337a5c1
Ben Jacobs <ben@benjacobs.co.uk>
2016-09-05 20:20:12 +0100

    Add base project info

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Pagekit\Application;
+
+return [
+
+    'name' => 'sermons',
+
+    'type' => 'extension',
+
+    // called when Pagekit initializes the module
+    'main' => function (Application $app) {
+        //todo: initialisation
+    }
+];
+
+?>
